1275d9ac16cf10f3b069c31e4bae03bf9e6c522e
ioanne <juanignaciobonini@hotmail.com>
2021-06-01 18:48:21 -0300

    ADB-001: The client is overwritten to return the new devices.

--- app/services/adb/client/client.py
+++ app/services/adb/client/client.py
@@ -1,5 +1,7 @@
 from ppadb.client import Client as AdbClient
 
+from app.services.adb.client.device import PoweredDevice
+
 ADB_CLIENT_IP = '127.0.0.1'
 ADB_CLIENT_PORT = 5037
 
@@ -12,3 +14,24 @@ def get_adb_client():
             port=ADB_CLIENT_PORT
         )
     return get_adb_client._cached_client
+
+
+class PoweredClient(AdbClient):
+    
+    def devices(self, state=None):
+        cmd = "host:devices"
+        result = self._execute_cmd(cmd)
+
+        devices = []
+
+        for line in result.split('\n'):
+            if not line:
+                break
+
+            tokens = line.split()
+            if state and len(tokens) > 1 and tokens[1] != state:
+                continue
+
+            devices.append(PoweredDevice(self, tokens[0]))
+
+        return devices
