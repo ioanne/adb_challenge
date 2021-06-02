4df9800cc0ab9966ac65fa248b7f1aea0046086f
ioanne <juanignaciobonini@hotmail.com>
2021-06-02 12:06:09 -0300

    ADB-003: Refactor functionalities. The library class is override to given it new functionalities.

--- app/services/adb/client/device.py
+++ app/services/adb/client/device.py
@@ -10,15 +10,15 @@ logger = logging.getLogger(__name__)
 class PoweredDevice(Device):
 
     def open_app(self, app):
-        "com.android.chrome/com.google.android.apps.chrome.Main"
-        self.shell(f'am start -n {app}')
+        """ Example: com.android.chrome/com.google.android.apps.chrome.Main"""
+        return self.shell(f'am start -n {app}')
 
     def get_log(self):
         return self.shell('logcat -d')
 
     def take_screen(self):
         timestamp = str(datetime.timestamp(datetime.now()))
-        file_name = f'/sdcard/screen-{timestamp}.png'
+        file_name = f'app/screen-{timestamp}.png'
         screen = self.screencap()
         with open(file_name, "wb") as fp:
             fp.write(screen)
