03ba4bdb0f07761c761d4a1d99a7be882609fb17
ioanne <juanignaciobonini@hotmail.com>
2021-06-02 11:48:29 -0300

    ADB-003: Add docstrings in endpoits.

--- app/api/v1/routers/device.py
+++ app/api/v1/routers/device.py
@@ -26,6 +26,9 @@ class DeviceViews:
         return get_adb_client()
 
     def _get_device(self, device_serial):
+        """Search and return a device.
+        If it does not exist an HTTPException is thrown.
+        :"""
         device = self.client.device(device_serial)
         if not device:
             raise HTTPException(status_code=404, detail='Device not found.')
@@ -33,14 +36,17 @@ class DeviceViews:
 
     @router.get("/devices/")
     async def get_devices(self):
+        """Endpoint to get all devices."""
         return self.client.devices() or []
 
     @router.get("/device/{device_serial}/")
     async def get_device(self, device_serial: str):
+        """Endpoint to get a specific device by serial."""
         return self._get_device(device_serial)
 
     @router.get("/device/{device_serial}/screenshot/")
     async def get_screenshot(self, device_serial: str):
+        """Endpoint to get a screen shot inside a device by serial."""
         device = self._get_device(device_serial)
         screen = device.screencap()
         if screen:
@@ -49,11 +55,13 @@ class DeviceViews:
 
     @router.get("/device/{device_serial}/logs/")
     async def get_logs(self, device_serial: str):
+        """Endpoint to get logs inside a device by serial."""
         device = self._get_device(device_serial)
         return device.get_log()
 
     @router.post("/device/{device_serial}/open_app/")
     async def open_app(self, device_serial: str, android_app: AndroidApp):
+        """Endpoint to open app inside a device by serial."""
         device = self._get_device(device_serial)
         message = device.open_app(android_app.app_id)
         if 'Error type' in message:
@@ -62,6 +70,7 @@ class DeviceViews:
 
     @router.post("/device/{device_serial}/install/")
     async def install_apk(self, device_serial: str, install_app_data: InstallAppData):
+        """Endpoint to install app inside a device by serial."""
         device = self._get_device(device_serial)
         try:
             was_installed = device.install(install_app_data.path)
