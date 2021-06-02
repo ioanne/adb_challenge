9534ea9c8b00fb34207aa7842e76591f8e5132eb
ioanne <juanignaciobonini@hotmail.com>
2021-06-02 11:37:30 -0300

    ADB-003: New endpoint to take screenshot.

--- app/api/v1/routers/device.py
+++ app/api/v1/routers/device.py
@@ -1,6 +1,8 @@
+import io
 from fastapi import HTTPException
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
+from starlette.responses import StreamingResponse
 
 from app.services.adb.client.powered_client import get_adb_client
 
@@ -14,13 +16,24 @@ class DeviceViews:
     def client(self):
         return get_adb_client()
 
+    def _get_device(self, device_serial):
+        device = self.client.device(device_serial)
+        if not device:
+            raise HTTPException(status_code=404, detail='Device not found.')
+        return device
+
     @router.get("/devices/")
     async def get_devices(self):
         return self.client.devices() or []
 
     @router.get("/device/{device_serial}/")
     async def get_device(self, device_serial: str):
-        device = self.client.device(device_serial)
-        if not device:
-            raise HTTPException(status_code=404, detail='Device not found.')
-        return device
+        return self._get_device(device_serial)
+
+    @router.get("/device/{device_serial}/screenshot/")
+    async def get_screenshot(self, device_serial: str):
+        device = self._get_device(device_serial)
+        screen = device.screencap()
+        if screen:
+            return StreamingResponse(io.BytesIO(screen), media_type="image/png")
+        return {}
